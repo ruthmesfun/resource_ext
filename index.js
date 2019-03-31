2b39da842f7e480a98aedc41bf4edd804ef311b2
ruthmesfun <ruth.mesfun@gmail.com>
2019-03-31 16:26:44 -0400

    Added logic for ela subjects

--- index.js
+++ index.js
@@ -1,58 +1,87 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const dropdown = document.getElementById('grades')
+    const grades = document.getElementById('grades')
+    const subjects = document.getElementById('subjects')
     const form = document.querySelector('form')
     const checkbox = document.getElementById('standards')
-    let standards = [];
+    let subject = 'ela'
+    let grade = 'k'
 
-    fetch('/math.json')
-    .then((r) => r.json())
-    .then(data => {
-        let standards = {}
+    let standards = []
 
-        for (let standard of data["LearningStandards"]["LearningStandardItem"])
-        {
-            if(standard["GradeLevels"]["GradeLevel"] === 'K'){
-                const ccss = standard["StatementCodes"]["StatementCode"]
-                // standards['K'][`${ccss}`] = standard["Statements"]["Statement"]
-                
-            }
-        }
-        
-    })
-    // event.target.body.innerHTML = standards
-
-    //EventListener for the dropdown to grab common core standards from grades
-
-    dropdown.addEventListener('change', (event) => {
-        const grade = event.target.value
-
-        fetch('/math.json')
-        .then((r) => r.json())
-        .then(data => {   
-            let checkBoxList = '';
-
-            for (let standard of data["LearningStandards"]["LearningStandardItem"])
-            {
-                if(standard["GradeLevels"]["GradeLevel"] === grade || standard["GradeLevels"]["GradeLevel"].includes(grade)){
-                    const ccss = standard["StatementCodes"]["StatementCode"].split('.')
-
-
-
-                    const parsedCCSS = ccss.slice(3,ccss.length).join('.')
-                    // standards['K'][`${ccss}`] = standard["Statements"]["Statement"]
-                    checkBoxList += `<option value= ${parsedCCSS} id= ${parsedCCSS} class= 'standards'>${parsedCCSS}</option>`
+    // Function that updates standards based on the grade and subject
+    const changeStandards = (grade, subject) => {
+        if(subject === 'math'){
+            fetch('/math.json')
+            .then((r) => r.json())
+            .then(data => {   
+                let checkBoxList = '';
+    
+                for (let standard of data["LearningStandards"]["LearningStandardItem"])
+                {
+                    if(standard["GradeLevels"]["GradeLevel"] === grade || standard["GradeLevels"]["GradeLevel"].includes(grade)){
+                        const ccss = standard["StatementCodes"]["StatementCode"].split('.')
+    
+    
+    
+                        const parsedCCSS = ccss.slice(3,ccss.length).join('.')
+                        // standards['K'][`${ccss}`] = standard["Statements"]["Statement"]
+                        checkBoxList += `<option value= ${parsedCCSS} id= ${parsedCCSS} class= 'standards'>${parsedCCSS}</option>`
+                    }
                 }
+                
+                document.querySelector('#standards').innerHTML = `<label for="exampleFormControlSelect2">Pick all the standards you need:</label>
+                <select multiple class="form-control" id="checkbox_grades" multiple >
+                </select>`
+                
+                document.querySelector('#checkbox_grades').innerHTML  = checkBoxList
+                
+            })
             }
-            
-            document.querySelector('#standards').innerHTML = `<label for="exampleFormControlSelect2">Pick all the standards you need:</label>
-            <select multiple class="form-control" id="checkbox_grades" multiple >
-            </select>`
-            
-            document.querySelector('#checkbox_grades').innerHTML  = checkBoxList
-            
-        })
-    } )
+            else{
+                fetch('/ela.json')
+                .then((r) => r.json())
+                .then(data => {   
+                    let checkBoxList = '';
+        
+                    for (let standard of data["LearningStandards"]["LearningStandardItem"])
+                    {
+                        if(standard["GradeLevels"]["GradeLevel"] === grade || standard["GradeLevels"]["GradeLevel"].includes(grade)){
+                            const ccss = standard["StatementCodes"]["StatementCode"].split('.')
+                            const parsedCCSS = ccss.slice(2,ccss.length).join('.')
+                            // standards['K'][`${ccss}`] = standard["Statements"]["Statement"]
+                            checkBoxList += `<option value= ${parsedCCSS} id= ${parsedCCSS} class= 'standards'>${parsedCCSS}</option>`
+                        }
+                    }
+                    
+                    document.querySelector('#standards').innerHTML = `<label for="exampleFormControlSelect2">Pick all the standards you need:</label>
+                    <select multiple class="form-control" id="checkbox_grades" multiple >
+                    </select>`
+                    
+                    document.querySelector('#checkbox_grades').innerHTML  = checkBoxList
+                    
+                })
+    
+            }
+    
+    }
 
+    //Updates standards based on updated grade
+    grades.addEventListener('change', (event) => {
+        grade = event.target.value
+        changeStandards(grade, subject)
+        
+
+    })
+
+    //Updates standards based on subject
+    subjects.addEventListener('change', event => {
+        subject = event.target.value
+
+        changeStandards(grade, subject)
+
+    })
+
+    //Adds standards in the standard array
     checkbox.addEventListener('click', (event) =>{
         let ccss = event.target.selectedOptions
 
@@ -64,6 +93,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     })
 
 
+    //submits for and redirct to TpT
     form.addEventListener('submit', (event) => {
         const standardsLink = standards.join(',')
 
